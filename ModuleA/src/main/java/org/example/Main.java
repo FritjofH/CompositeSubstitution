851f8ed5c42e1aa0343bc0fafe8fea16a327c1a3
Fritjof Höst <fritjof.host@digitalroute.com>
2023-01-04 19:26:11 +0100

    MCVE

---
+++ ModuleA/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.example.StringGetter;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(StringGetter.getString());
+    }
+}
---
+++ Tooling/example/src/main/java/org/example/StringGetter.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class StringGetter {
+    public static String getString() {
+        return "This is a string!";
+    }
+}
